c4c5aabcabf2dd3e514a00fd9eec767e261f5066
JISHNUSHAJI <jishnushaji1415@gmail.com>
2016-11-02 18:32:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,1 @@
 
-var button = document.querySelector('.read-more');
-
-button.addEventListener('click', function(event) {
-  var span = event.target.previousElementSibling.querySelector('span');
-  span.classList.add('fade-in');
-});
-.show-more span {
-  display: inline-block;
-  height: 0;
-  overflow: hidden;
-  transition: opacity 2s;
-  opacity: 0;
-}
-.show-more span.fade-in {
-  height: auto;
-  opacity: 1;
-}
