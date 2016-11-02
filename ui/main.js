22594d128c9fe7fd14b35b52e48d823b5d721a51
JISHNUSHAJI <jishnushaji1415@gmail.com>
2016-11-02 17:34:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var span = document.getElementsByTagName('qw')[0];
 var hideshow = document.getElementById('hideshow');
 
-qw.style.display = 'none';
+span.style.display = 'none';
 
 hideshow.onclick = function() {
-  qw.style.display = 'block';
+  span.style.display = 'block';
 };
