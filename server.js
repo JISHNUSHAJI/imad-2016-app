40ad9703ea6b5644832bc8a6b57cf58beb255e53
JISHNUSHAJI <jishnushaji1415@gmail.com>
2016-11-04 20:29:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,10 +19,17 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/DSC03948.JPG', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'DSC03948.JPG'));
 });
+
 app.get('/ui/DSC03995.JPG', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'DSC03995.JPG'));
 });
+app.get('/DSC03959.JPG', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'DSC03959.JPG'));
+});
 
+app.get('/ui/DSC03995.JPG', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'DSC03995.JPG'));
+});
 
 
 
